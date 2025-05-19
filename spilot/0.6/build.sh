cbf504876daf0000fcde2810a29c21a32ee80d30
Xdydy <dydy@i2ec.top>
2025-05-19 22:37:02 +0800

    fix: add host

--- spilot/0.6/build.sh
+++ spilot/0.6/build.sh
@@ -3,4 +3,4 @@ version="0.6"
 ip=`hostname -i`
 echo "Building faasit-spilot image with IP: $ip"
 # Build the Docker image with the specified IP address
-docker build --build-arg IP=$ip --no-cache -t faasit-spilot:$version .
+docker build --network host --build-arg IP=$ip --no-cache -t faasit-spilot:$version .
